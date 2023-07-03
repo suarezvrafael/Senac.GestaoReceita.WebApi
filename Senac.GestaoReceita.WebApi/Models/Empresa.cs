2e11e60662e590673380727bbc1e7b3a4dafc0d2
Daniel <danielkloh06@gmail.com>
2023-07-03 20:59:44 -0300

    Corrigindo identação

--- Senac.GestaoReceita.WebApi/Models/Empresa.cs
+++ Senac.GestaoReceita.WebApi/Models/Empresa.cs
@@ -9,59 +9,43 @@ namespace Senac.GestaoReceita.WebApi.Models
         [Key]
         public int id { get; set; }
 
-
-        [StringLength(14)]//
+        [StringLength(18)]//
         public string CNPJ { get; set; }
-
 
         [StringLength(150)]
         public string razaoSosial{ get; set; }
 
-
         [StringLength(150)]
         public string rua { get; set; }
-
 
         [StringLength(150)]
         public string bairro{ get; set; }
 
-
-        [StringLength(5)]//
+        [StringLength(11)]//
         public int numeroEndereco { get; set; }
-
 
         [StringLength(200)]
         public string complemento{ get; set; }
 
-
-        [StringLength(200)]
+        [StringLength(90)]
         public string email{ get; set; }
 
-
-        [StringLength(14)]
+        [StringLength(15)]
         public string telefone { get; set; }
-
 
         [StringLength(150)]
         public string nomeFantasia { get; set; }
 
-
         [StringLength(11)]//
         public int idcidade { get; set; }
-
         public Cidade cidade { get; set; }
-
-
 
         public DateTime createEmpresa { get; set; }
 
-
         public DateTime updateEmpresa { get; set; }
-
-
-
 
         [StringLength(11)]
         public int idUsername { get; set; }
+        //public Cidade cidade { get; set; }
     }
 }
