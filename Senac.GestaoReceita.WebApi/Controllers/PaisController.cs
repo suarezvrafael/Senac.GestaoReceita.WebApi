27a2ca33162210f28147cd3dc7fd0ffe7a9a9812
Lorenzo <locod4mw@gmail.com>
2023-07-10 20:31:22 -0300

    criação das controller pais e cidade

---
+++ Senac.GestaoReceita.WebApi/Controllers/PaisController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Senac.GestaoReceita.WebApi.Data;
+using Senac.GestaoReceita.WebApi.Models;
+
+namespace Senac.GestaoReceita.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PaisController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pais
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pais>>> GetPaises()
+        {
+          if (_context.Paises == null)
+          {
+              return NotFound();
+          }
+            return await _context.Paises.ToListAsync();
+        }
+
+        // GET: api/Pais/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pais>> GetPais(int id)
+        {
+          if (_context.Paises == null)
+          {
+              return NotFound();
+          }
+            var pais = await _context.Paises.FindAsync(id);
+
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return pais;
+        }
+
+        // PUT: api/Pais/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPais(int id, Pais pais)
+        {
+            if (id != pais.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pais).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pais
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pais>> PostPais(Pais pais)
+        {
+          if (_context.Paises == null)
+          {
+              return Problem("Entity set 'AppDbContext.Paises'  is null.");
+          }
+            _context.Paises.Add(pais);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPais", new { id = pais.Id }, pais);
+        }
+
+        // DELETE: api/Pais/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePais(int id)
+        {
+            if (_context.Paises == null)
+            {
+                return NotFound();
+            }
+            var pais = await _context.Paises.FindAsync(id);
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            _context.Paises.Remove(pais);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaisExists(int id)
+        {
+            return (_context.Paises?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
