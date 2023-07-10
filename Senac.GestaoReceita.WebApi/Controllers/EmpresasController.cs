e23506b623bc6c662b16090b8488d372a677f153
Daniel <danielkloh06@gmail.com>
2023-07-10 19:50:26 -0300

    Criado controller da cadEmpresa

---
+++ Senac.GestaoReceita.WebApi/Controllers/EmpresasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Senac.GestaoReceita.WebApi.Data;
+using Senac.GestaoReceita.WebApi.Models;
+
+namespace Senac.GestaoReceita.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmpresasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empresas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empresa>>> GetEmpresas()
+        {
+          if (_context.Empresas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Empresas.ToListAsync();
+        }
+
+        // GET: api/Empresas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empresa>> GetEmpresa(int id)
+        {
+          if (_context.Empresas == null)
+          {
+              return NotFound();
+          }
+            var empresa = await _context.Empresas.FindAsync(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            return empresa;
+        }
+
+        // PUT: api/Empresas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpresa(int id, Empresa empresa)
+        {
+            if (id != empresa.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empresa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpresaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Empresas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Empresa>> PostEmpresa(Empresa empresa)
+        {
+          if (_context.Empresas == null)
+          {
+              return Problem("Entity set 'AppDbContext.Empresas'  is null.");
+          }
+            _context.Empresas.Add(empresa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmpresa", new { id = empresa.id }, empresa);
+        }
+
+        // DELETE: api/Empresas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpresa(int id)
+        {
+            if (_context.Empresas == null)
+            {
+                return NotFound();
+            }
+            var empresa = await _context.Empresas.FindAsync(id);
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empresas.Remove(empresa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmpresaExists(int id)
+        {
+            return (_context.Empresas?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
